40f0d7fb96eda5dcb40b6e04467d5d75c3ca8b59
ErikFeeley <ehfeeley@gmail.com>
2017-11-19 21:01:51 -0500

    back in the saddle

---
+++ DotnetBackend/Conduit/Features/Tests/TestsController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Conduit.Features.Tests
+{
+    [Route("api/[controller]")]
+    public class TestsController
+    {
+        public IActionResult Index() => new OkObjectResult("hi");
+    }
+}
